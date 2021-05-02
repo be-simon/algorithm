797bdf567d0d7eaf83bb2028b9ceaf46a013bb17
정재용 <wodydthk7@hanyang.ac.kr>
2021-05-02 21:43:28 +0900

    21.05.02 kakao intern bfs

--- programmers/2020kakao_intern_road.py
+++ programmers/2020kakao_intern_road.py
@@ -1,37 +1,38 @@
+from collections import deque
+
 dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+dy = [0, 1, 0, -1]
 
 def solution(board):
-    answer = 0
     N = len(board)
-    memo = [[[0] for _ in range(N)] for _ in range(N)]    
-    memo[0][1] = [100, (-1, 0)]
-    memo[1][0] = [100, (0, -1)]
+    visit = [[0 for _ in range(N)] for _ in range(N)]
+    result = []
     
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if (i, j) == (0, 1) or (i, j) == (1, 0):
-                continue
-            if board[i][j] == 0:
-                # 인접 셀에서의 추가 비용 계산
-                value = []
-                for x, y in zip(dx, dy):
-                    adx = j + x
-                    ady = i + y
-                    if adx >= 0 and adx < N and ady >= 0 and ady < N:
-                        print(ady, adx)
-                        if board[ady][adx] != 1 and memo[ady][adx][0] > 0:
-                            if memo[ady][adx][1] == (x, y):
-                                value.append([memo[ady][adx][0] + 100, (x, y)])
-                            else:
-                                value.append([memo[ady][adx][0] + 600, (x, y)])
-                print(i, j, value)
-                value.sort(key = lambda x: x[0])
-                memo[i][j] = value[0]
-
-    # print(memo)
-    answer = memo[N - 1][N - 1][0]          
-    return answer
+    queue = deque([[0, 0, 0, -1]]) # x, y, cost, dir
+    while len(queue) > 0:
+        x, y, cost, drt = queue.popleft()
+        # print(x, y, cost, drt)
+        if (x, y) == (N - 1, N - 1):
+            result.append(cost)
+            continue
+            
+        for i in range(len(dx)):
+            adx = x + dx[i]
+            ady = y + dy[i]
+            if 0 <= adx < N and 0 <= ady < N and board[ady][adx] == 0:
+                _cost = cost
+                if drt == -1 or (drt - i) % 2 == 0:
+                    _cost += 100    
+                else:
+                    _cost += 600
+                    
+                if visit[ady][adx] == 0 or visit[ady][adx] >= _cost:
+                    queue.append([adx, ady, _cost, i])
+                    visit[ady][adx] = _cost
+    
+    print(result)
+    result.sort()
+    return result[0]
 
 # print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), 900)
 print(solution([[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 1], [0, 0, 1, 0, 0, 0, 1, 0], [0, 1, 0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0]]), 3800)
